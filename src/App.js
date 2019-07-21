594cd66b2ff9380f632d48f9803ab5ceab9cfe7b
ianchiu28 <nk9710632@gmail.com>
2019-07-21 14:03:49 +0800

    board final optimize

--- src/App.js
+++ src/App.js
@@ -27,61 +27,22 @@ class Board extends React.Component {
   render() {
     const current = this.state.board.slice();
 
-    let line1 = [], line2 = [], line3 = [], line4 = [], line5 = [], line6 = [], line7 = [], line8 = [], line9 = [];
+    // draw the board
+    let board = [], line = [];
     for (let i = 0; i < current.length; i++) {
-      let row = parseInt(i / 9);
-      switch (row) {
-        case 0:
-          line1.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 1:
-          line2.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 2:
-          line3.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 3:
-          line4.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 4:
-          line5.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 5:
-          line6.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 6:
-          line7.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 7:
-          line8.push(<Block value={this.coordinate(i)}/>);
-          break;
-        case 8:
-          line9.push(<Block value={this.coordinate(i)}/>);
-          break;
+      line.push(<Block value={this.coordinate(i)}/>);
+
+      // new line
+      let newLineIndex = i % 9;
+      if (newLineIndex === 8) {
+        board.push(<div className='d-flex flex-row'>{line}</div>);
+        line = [];
       }
     }
 
-    let newLine1 = <div className='d-flex flex-row'>{line1}</div>;
-    let newLine2 = <div className='d-flex flex-row'>{line2}</div>;
-    let newLine3 = <div className='d-flex flex-row'>{line3}</div>;
-    let newLine4 = <div className='d-flex flex-row'>{line4}</div>;
-    let newLine5 = <div className='d-flex flex-row'>{line5}</div>;
-    let newLine6 = <div className='d-flex flex-row'>{line6}</div>;
-    let newLine7 = <div className='d-flex flex-row'>{line7}</div>;
-    let newLine8 = <div className='d-flex flex-row'>{line8}</div>;
-    let newLine9 = <div className='d-flex flex-row'>{line9}</div>;
-
     return (
       <div>
-        {newLine1}
-        {newLine2}
-        {newLine3}
-        {newLine4}
-        {newLine5}
-        {newLine6}
-        {newLine7}
-        {newLine8}
-        {newLine9}
+        {board}
       </div>
     );
   }
