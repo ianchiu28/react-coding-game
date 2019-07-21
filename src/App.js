3d4f09d4e7c8195a57630a83ff5479d915405824
ianchiu28 <nk9710632@gmail.com>
2019-07-21 19:10:02 +0800

    tab 4 space

--- src/App.js
+++ src/App.js
@@ -5,256 +5,249 @@ import './App.css';
 const delayTime = 1000;
 
 class Block extends React.Component {
-  render() {
-    return (
-      <div className='border border-dark rounded'>
-        {this.props.value + '(' + this.props.coordinateX + ',' + this.props.coordinateY + ')'}
-      </div>
-    );
-  }
+	render() {
+    	return (
+      		<div className='border border-dark rounded'>
+        		{this.props.value + '(' + this.props.coordinateX + ',' + this.props.coordinateY + ')'}
+      		</div>
+    	);
+  	}
 }
 
 class Board extends React.Component {
-  coordinateX(position) {
-    return (' ' + (-4 + (position % 9)).toString()).slice(-2);
-  }
+	coordinateX(position) {
+		return (' ' + (-4 + (position % 9)).toString()).slice(-2);
+	}
 
-  coordinateY(position) {
-    return (' ' + (4 - parseInt(position / 9)).toString()).slice(-2);
-  }
+	coordinateY(position) {
+		return (' ' + (4 - parseInt(position / 9)).toString()).slice(-2);
+	}
 
-  drawBoard(current) {
-    // draw the board
-    let board = [], line = [];
-    for (let i = 0; i < current.length; i++) {
-      line.push(
-        <Block
-          key={i.toString()}
-          value={current[i]}
-          coordinateX={this.coordinateX(i)}
-          coordinateY={this.coordinateY(i)}
-        />
-      );
+	drawBoard(current) {
+		// draw the board
+		let board = [], line = [];
+		for (let i = 0; i < current.length; i++) {
+			line.push(
+				<Block
+					key={i.toString()}
+					value={current[i]}
+					coordinateX={this.coordinateX(i)}
+					coordinateY={this.coordinateY(i)}
+				/>
+			);
 
-      // new line
-      let newLineIndex = i % 9;
-      if (newLineIndex === 8) {
-        board.push(<div key={i.toString()} className='d-flex flex-row'>{line}</div>);
-        line = [];
-      }
-    }
-    return board;
-  }
+			// new line
+			let newLineIndex = i % 9;
+			if (newLineIndex === 8) {
+				board.push(<div key={i.toString()} className='d-flex flex-row'>{line}</div>);
+				line = [];
+			}
+		}
+		return board;
+	}
 
-  render() {
-    const current = this.props.board.slice();
+	render() {
+		const current = this.props.board.slice();
 
-    return (
-      <div>
-        {this.drawBoard(current)}
-      </div>
-    );
-  }
+		return (
+			<div>
+				{this.drawBoard(current)}
+			</div>
+		);
+	}
 }
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleButtonOnClick = this.handleButtonOnClick.bind(this);
-    this.state = {
-      board: [
-        'A', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'B',
-        'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-        'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-        'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-        'r', 'r', 'r', 'r', 'c', 'r', 'r', 'r', 'r',
-        'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-        'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-        'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
-        'C', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'D'
-      ],
-      direction: 'N',
-      script: [
-        'move',
-        'turnLeft',
-        'move',
-        'turnRight',
-        'move',
-        'move'
-      ]
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.handleButtonOnClick = this.handleButtonOnClick.bind(this);
+		this.state = {
+			board: [
+				'A', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'B',
+				'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+				'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+				'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+				'r', 'r', 'r', 'r', 'c', 'r', 'r', 'r', 'r',
+				'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+				'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+				'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r',
+				'C', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'D'
+			],
+			direction: 'N',
+			script: props.script
+		}
+	}
 
-  actionMove() {
-    // calculate where to go
-    let carIndex = this.state.board.findIndex(e => e === 'c');
-    let direction = this.state.direction;
-    let newCarIndex;
-    switch (direction) {
-      case 'N':
-        newCarIndex = carIndex - 9;
+	actionMove() {
+		// calculate where to go
+		let carIndex = this.state.board.findIndex(e => e === 'c');
+		let direction = this.state.direction;
+		let newCarIndex;
+		switch (direction) {
+			case 'N':
+				newCarIndex = carIndex - 9;
 
-        // hit wall
-        if (newCarIndex < 0) {
-          return true;
-        }
-        break;
-      case 'S':
-        newCarIndex = carIndex + 9;
+				// hit wall
+				if (newCarIndex < 0) {
+					return true;
+				}
+				break;
+			case 'S':
+				newCarIndex = carIndex + 9;
 
-        // hit wall
-        if (newCarIndex > 80) {
-          return true;
-        }
-        break;
-      case 'E':
-        newCarIndex = carIndex + 1;
+				// hit wall
+				if (newCarIndex > 80) {
+					return true;
+				}
+				break;
+			case 'E':
+				newCarIndex = carIndex + 1;
 
-        // hit wall
-        if (newCarIndex % 9 === 0) {
-          return true;
-        }
-        break;
-      case 'W':
-        newCarIndex = carIndex - 1;
+				// hit wall
+				if (newCarIndex % 9 === 0) {
+					return true;
+				}
+				break;
+			case 'W':
+				newCarIndex = carIndex - 1;
 
-        // hit wall
-        if (newCarIndex % 9 === 8) {
-          return true;
-        }
-        break;
-      default:
-        alert('Error: undefind direction!');
-        return;
-    }
+				// hit wall
+				if (newCarIndex % 9 === 8) {
+					return true;
+				}
+				break;
+			default:
+				alert('Error: undefind direction!');
+				return;
+		}
 
-    // update board
-    let newBoard = this.state.board.slice();
-    let tmp = newBoard[carIndex];
-    newBoard[carIndex] = newBoard[newCarIndex];
-    newBoard[newCarIndex] = tmp;
-    this.setState({
-      board: newBoard
-    });
-  }
+		// update board
+		let newBoard = this.state.board.slice();
+		let tmp = newBoard[carIndex];
+		newBoard[carIndex] = newBoard[newCarIndex];
+		newBoard[newCarIndex] = tmp;
+		this.setState({
+			board: newBoard
+		});
+	}
 
-  actionTurnLeft() {
-    // calculate where to go
-    let direction = this.state.direction;
-    let newDirection;
-    switch (direction) {
-      case 'N':
-        newDirection = 'W';
-        break;
-      case 'S':
-          newDirection = 'E';
-        break;
-      case 'E':
-          newDirection = 'N';
-        break;
-      case 'W':
-          newDirection = 'S';
-        break;
-      default:
-        alert('Error: undefind direction!');
-    }
+	actionTurnLeft() {
+		// calculate where to go
+		let direction = this.state.direction;
+		let newDirection;
+		switch (direction) {
+			case 'N':
+				newDirection = 'W';
+        		break;
+      		case 'S':
+        	  	newDirection = 'E';
+        		break;
+      		case 'E':
+          		newDirection = 'N';
+        		break;
+      		case 'W':
+          		newDirection = 'S';
+        		break;
+      		default:
+        		alert('Error: undefind direction!');
+    	}
 
-    // update direction
-    this.setState({
-      direction: newDirection
-    });
-  }
+    	// update direction
+    	this.setState({
+      		direction: newDirection
+    	});
+  	}
 
-  actionTurnRight() {
-    // calculate where to go
-    let direction = this.state.direction;
-    let newDirection;
-    switch (direction) {
-      case 'N':
-        newDirection = 'E';
-        break;
-      case 'S':
-          newDirection = 'W';
-        break;
-      case 'E':
-          newDirection = 'S';
-        break;
-      case 'W':
-          newDirection = 'N';
-        break;
-      default:
-        alert('Error: undefind direction!');
-    }
+  	actionTurnRight() {
+    	// calculate where to go
+    	let direction = this.state.direction;
+    	let newDirection;
+    	switch (direction) {
+      		case 'N':
+        		newDirection = 'E';
+        		break;
+      		case 'S':
+          		newDirection = 'W';
+        		break;
+      		case 'E':
+          		newDirection = 'S';
+        		break;
+      		case 'W':
+          		newDirection = 'N';
+        		break;
+      		default:
+        		alert('Error: undefind direction!');
+    	}
 
-    // update direction
-    this.setState({
-      direction: newDirection
-    });
-  }
+    	// update direction
+    	this.setState({
+      		direction: newDirection
+    	});
+  	}
 
-  runScript() {
-    let script = this.state.script.slice();
+  	runScript() {
+	    let script = this.state.script.slice();
 
-    // stop interval
-    if (script.length === 0) {
-      clearInterval(this.timerId);
-      return;
-    }
+    	// stop interval
+    	if (script.length === 0) {
+      		clearInterval(this.timerId);
+      		return;
+    	}
 
-    // run the first command
-    const command = script.shift();
-    let isHitWall;
-    switch (command) {
-      case 'move':
-        isHitWall = this.actionMove();
-        break;
-      case 'turnLeft':
-        this.actionTurnLeft();
-        break;
-      case 'turnRight':
-        this.actionTurnRight();
-        break;
-      default:
-        alert('Error: undefined script!');
-    }
+    	// run the first command
+    	const command = script.shift();
+    	let isHitWall;
+    	switch (command) {
+      		case 'move':
+        		isHitWall = this.actionMove();
+        		break;
+      		case 'turnLeft':
+        		this.actionTurnLeft();
+        		break;
+      		case 'turnRight':
+        		this.actionTurnRight();
+        		break;
+      		default:
+        		alert('Error: undefined script!');
+    	}
 
-    // hit the wall, game over
-    if (isHitWall) {
-      alert('Game Over!');
-      script = [];
-    }
+    	// hit the wall, game over
+    	if (isHitWall) {
+      		alert('Game Over!');
+      		script = [];
+    	}
 
-    // update script
-    this.setState({
-      script: script
-    });
-  }
+    	// update script
+    	this.setState({
+      		script: script
+    	});
+  	}
 
-  handleButtonOnClick() {
-    // start interval, run a command every ${delayTime} ms
-    this.timerId = setInterval(() => this.runScript(), delayTime);
-  }
+  	handleButtonOnClick() {
+	    // start interval, run a command every ${delayTime} ms
+	    this.timerId = setInterval(() => this.runScript(), delayTime);
+  	}
 
-  render() {
-    return (
-      <div className='game'>
-        <div className='game-info'>
-          <h1>this is game-info</h1>
-          <h1>Direction: {this.state.direction}</h1>
-          <button onClick={this.handleButtonOnClick}>Go!</button>
-        </div>
-        <div className='game-board'>
-          <Board board={this.state.board}/>
-        </div>
-      </div>
-    );
-  }
+	render() {
+	    return (
+      		<div className='game'>
+        		<div className='game-info'>
+          			<h1>this is game-info</h1>
+          			<h1>Direction: {this.state.direction}</h1>
+          			<button onClick={this.handleButtonOnClick}>Go!</button>
+        		</div>
+        		<div className='game-board'>
+          			<Board board={this.state.board} />
+        		</div>
+      		</div>
+    	);
+  	}
 }
 
 function App() {
-  return (
-    <Game />
-  );
+	return (
+	    <Game />
+  	);
 
   // return (
   //   <div className="App">
